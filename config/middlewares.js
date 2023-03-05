d9cedceb099e3296125484e6d9db91cc69c72bbb
Hanif Bahari <hanifbahari@Hanifs-MacBook-Pro-2.local>
2023-03-05 06:20:23 +0100

    cors modified

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,23 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: [
+        "http://localhost:1337",
+        "https://capable-biscuit-2e11bd.netlify.app/",
+      ],
+    },
+  },
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
